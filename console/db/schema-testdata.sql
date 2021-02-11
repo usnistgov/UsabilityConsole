45aaed6b0bccb970828b457e1eca1667153d9bea
Kevin Mangold <kevin.mangold@nist.gov>
2021-02-11 14:51:01 -0500

    Update schema-testdata.sql
    cleaning out test data

--- console/db/schema-testdata.sql
+++ console/db/schema-testdata.sql
@@ -2,23 +2,21 @@
     TEST DATA
 ***********************************************************************************************************************/
 
-INSERT INTO users (role_id, username, password, first_name, last_name, api_key) VALUES (1, 'kmangold', 'p4ssw0rd', 'Kevin', 'Mangold', "asdf1234");      -- user_id: 2
-INSERT INTO users (role_id, username, password, first_name, last_name, api_key) VALUES (2, 'testuser', 'p4ssw0rd', 'Test', 'User', 'qwerty12345678');          -- user_id: 3
-INSERT INTO users (role_id, username, password, first_name, last_name, api_key) VALUES (1, 'js1', 'p4ssw0rd', 'John', 'Smith', 'qwerty12345678');              -- user_id: 4
-INSERT INTO users (role_id, username, password, first_name, last_name) VALUES (2, 'alice', 'p4ssw0rd', 'Alice', 'Cooper');          -- user_id: 5
-INSERT INTO users (role_id, username, password, first_name, last_name) VALUES (2, 'rmcdonald', 'p4ssw0rd', 'Ronald', 'McDonald');   -- user_id: 6
+INSERT INTO users (role_id, username, password, first_name, last_name, api_key) VALUES (2, 'testuser', 'p4ssw0rd', 'Test', 'User', 'qwerty12345678');          -- user_id: 2
+INSERT INTO users (role_id, username, password, first_name, last_name, api_key) VALUES (1, 'js1', 'p4ssw0rd', 'John', 'Smith', 'qwerty12345678');              -- user_id: 3
+INSERT INTO users (role_id, username, password, first_name, last_name) VALUES (2, 'alice', 'p4ssw0rd', 'Alice', 'Cooper');          -- user_id: 4
+INSERT INTO users (role_id, username, password, first_name, last_name) VALUES (2, 'rmcdonald', 'p4ssw0rd', 'Ronald', 'McDonald');   -- user_id: 5
 
 INSERT INTO security_answers (question_id, user_id, answer) VALUES 
 (1, 2,'answer1'), (5, 2,'answer2'), (9, 2,'answer3'),
 (1, 3,'answer1'), (5, 3,'answer2'), (9, 3,'answer3'),
 (1, 4,'answer1'), (5, 4,'answer2'), (9, 4,'answer3'),
 (1, 5,'answer1'), (5, 5,'answer2'), (9, 5,'answer3'),
-(1, 6,'answer1'), (5, 6,'answer2'), (9, 6,'answer3'),
 (1, 1,'john doe'),(5, 1,'pizzaparadise'),(9, 1,'petone');
 
 
 INSERT INTO sessions (creator_id, name) VALUES (2, "Test Study 1 - P037");      -- session_id: 1
-INSERT INTO sessions (creator_id, name) VALUES (5, "Alice's Study - P314");     -- session_id: 2
+INSERT INTO sessions (creator_id, name) VALUES (4, "Alice's Study - P314");     -- session_id: 2
 
 INSERT INTO entries (session_id, creator_id, value) VALUES (1, 2, "Entry 1-1");       -- entry_id: 1
 INSERT INTO entries (session_id, creator_id, value) VALUES (1, 3, "Entry 1-2");       -- entry_id: 2
@@ -27,4 +25,3 @@ INSERT INTO entries (session_id, creator_id, value) VALUES (2, 5, "Entry 2-2"); 
 INSERT INTO entries (session_id, creator_id, value) VALUES (1, 5, "Entry 1-3");       -- entry_id: 5
 INSERT INTO entries (session_id, creator_id, value) VALUES (2, 2, "Entry 2-3");       -- entry_id: 6
 INSERT INTO entries (session_id, creator_id, value) VALUES (1, 4, "Entry 1-4");       -- entry_id: 7
-INSERT INTO entries (session_id, creator_id, value) VALUES (1, 6, "Entry 1-5");       -- entry_id: 8
